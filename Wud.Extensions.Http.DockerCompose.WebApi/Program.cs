e214b97cbceac4b0c205141bb9e1c49d035154bf
Ankit Mehta <10181117+ankitmehtame@users.noreply.github.com>
2024-01-02 22:13:04 +0800

    Update services, add httpclient

--- Wud.Extensions.Http.DockerCompose.WebApi/Program.cs
+++ Wud.Extensions.Http.DockerCompose.WebApi/Program.cs
@@ -6,17 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = AssemblyUtils.AssemblyVersion,
-                    Title = "WUD Extensions - Http Docker Compose",
-                    Description = "v" + AssemblyUtils.InfoVersion
-                }));
+{
+    Version = AssemblyUtils.AssemblyVersion,
+    Title = "WUD Extensions - Http Docker Compose",
+    Description = "v" + AssemblyUtils.InfoVersion
+}));
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.UpdateSerializerOptions();
 });
+builder.Services.AddHttpClient();
 builder.Services.AddSingleton<IEnvironmentProvider, EnvironmentProvider>();
 builder.Services.AddSingleton<IDockerComposeUtility, DockerComposeUtility>();
+builder.Services.AddSingleton<IWudService, WudService>();
+builder.Services.AddSingleton<ContainerApis>();
 
 var app = builder.Build();
 
