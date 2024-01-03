444350a0c9f15aace48dd6e8dd979f951ae7c094
Ankit Mehta <10181117+ankitmehtame@users.noreply.github.com>
2024-01-03 16:43:50 +0800

    Use new lib to see if version can be propagated

--- Wud.Extensions.Http.DockerCompose.WebApi/AssemblyUtils.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Wud.Extensions.Http.DockerCompose.WebApi;
-
-public static class AssemblyUtils
-{
-    public static readonly string InfoVersion = ThisAssembly.AssemblyInformationalVersion;
-    public static readonly string AssemblyVersion = ThisAssembly.AssemblyVersion;
-}
