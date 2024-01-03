9123e14dc08102d38b98b0d8e591b1393bfab4db
Ankit Mehta <10181117+ankitmehtame@users.noreply.github.com>
2024-01-03 16:43:50 +0800

    Use new lib to see if version can be propagated

---
+++ Wud.Extensions.Http.DockerCompose.WebApi/VersionUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Semver;
+namespace Wud.Extensions.Http.DockerCompose.WebApi;
+
+public static class VersionUtils
+{
+    private static readonly Lazy<SemVersion> _version = new Lazy<SemVersion>(() =>
+    {
+        string versionFileText = Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? string.Empty;
+        if (!SemVersion.TryParse(versionFileText.Trim(), SemVersionStyles.Any, out var semverFile))
+        {
+            return SemVersion.FromVersion(new Version());
+        }
+        return semverFile;   
+    });
+
+    public static readonly string InfoVersion = _version.Value.ToString();
+    public static readonly string AssemblyVersion = _version.Value.WithoutMetadata().ToVersion().ToString(3);
+}
