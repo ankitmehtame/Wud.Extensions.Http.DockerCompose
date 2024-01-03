cc9141c77aa15eb9441728b627e94af66a847872
Ankit Mehta <10181117+ankitmehtame@users.noreply.github.com>
2024-01-03 12:07:01 +0800

    Retrieve version from dynamically generated code

--- Wud.Extensions.Http.DockerCompose.WebApi/AssemblyUtils.cs
+++ Wud.Extensions.Http.DockerCompose.WebApi/AssemblyUtils.cs
@@ -1,9 +1,7 @@
-﻿using System.Reflection;
-
-namespace Wud.Extensions.Http.DockerCompose.WebApi;
+﻿namespace Wud.Extensions.Http.DockerCompose.WebApi;
 
 public static class AssemblyUtils
 {
-    public static string InfoVersion = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
-    public static string AssemblyVersion = Assembly.GetEntryAssembly().GetName().Version.ToString(3);
+    public static readonly string InfoVersion = ThisAssembly.AssemblyInformationalVersion;
+    public static readonly string AssemblyVersion = ThisAssembly.AssemblyVersion;
 }
